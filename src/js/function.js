0cec37ca167afeb6cb0653a407d00e00ec584439
Romain Dehasseleer <romain.dehasseleer@protonmail.com>
2018-07-16 00:18:21 +0200

    Integration basic of the models (keyword and search), small modification of the site (always also ugly) and reorganization of the web files (json & html), addition of some javascript libraries (search and tree D3)

--- src/js/function.js
+++ src/js/function.js
@@ -14,3 +14,31 @@ function parseJSON(file) {
 function wrapWord(word) {
     return word.replace(/ .*/, '');
 }
+// Shortchut of console.log
+function l(text) {
+    console.log(text);
+}
+// Remove double quote from string object
+function clearString(string) {
+    return string.replace(/['"]+/g, '');
+}
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", "js/json/"+file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+function cosElementCircle(i,length,width){
+    return (radius * Math.cos((i / (length / 2)) * Math.PI)) + (width / 2);
+}
+
+function sinElementCircle(i,length,width){
+    return (radius * Math.sin((i / (length / 2)) * Math.PI)) + (width / 2);
+}
