2fe6bb0ede389f62d50cc84de23a3071cd35cf93
Ilion Beyst <ilion.beyst@gmail.com>
2018-07-16 14:15:25 +0200

    extract localize_text to utils module

--- backend/frisr3/organisations.py
+++ backend/frisr3/organisations.py
@@ -1,4 +1,5 @@
 import zeep
+from frisr3.fris_utils import localize_text
 
 SCHEME = 'https://frisr3.researchportal.be/ws/OrganisationServiceFRIS?wsdl'
 SETTINGS = zeep.Settings(strict=False)
@@ -6,14 +7,17 @@ SETTINGS = zeep.Settings(strict=False)
 class Organisation:
     def __init__(self, data):
         self.data = data
-    
+
     @property
     def uuid(self):
         return self.data.uuid
-
+    
     @property
     def name(self):
         return self.localized_name()
+
+    def localized_name(self, locale='en'):
+        return localize_text(self.data.name, locale)
 
     @property
     def acronym(self):
@@ -22,27 +26,17 @@ class Organisation:
     @property
     def keywords(self):
         return self.localized_keywords()
-    
-    @property
-    def research_activity(self):
-        return self.localized_research_activity()
-    
-    def localized_name(self, locale='en'):
-        text = self.data.name
-        if not text:
-            return None
-        return localize_text(text, locale)
 
     def localized_keywords(self, locale='en'):
         keywords = self.data.keywords.keyword
         return [k._value_1 for k in keywords if k.locale == locale]
-    
-    def localized_research_activity(self, locale='en'):
-        text = self.data.researchActivity
-        if not text:
-            return None
-        return localize_text(text, locale) 
 
+    @property
+    def research_activity(self):
+        return self.localized_research_activity()
+
+    def localized_research_activity(self, locale='en'):
+        return localize_text(self.data.researchActivity, locale) 
 
 
 class OrganisationService:
@@ -56,11 +50,3 @@ class OrganisationService:
         if len(response.organisation) > 0:
             data = response.organisation[0]
             return Organisation(data)
-
-
-def localize_text(localized_text, locale='en'):
-    """ Get a value from a FRIS LocalizedText object """
-    for text in localized_text.texts.text:
-        if text.locale == locale:
-            return text._value_1
-    return None
